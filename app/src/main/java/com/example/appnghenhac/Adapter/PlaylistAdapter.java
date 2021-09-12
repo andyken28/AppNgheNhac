07b8359052b38625320fad5782cd85f4fd56c0c6
unknown <letoan0311@gmail.com>
2021-09-12 20:25:57 +0700

    second

---
+++ app/src/main/java/com/example/appnghenhac/Adapter/PlaylistAdapter.java
@@ -0,0 +1,54 @@
+package com.example.appnghenhac.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.appnghenhac.Model.Playlist;
+import com.example.appnghenhac.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PlaylistAdapter extends ArrayAdapter<Playlist> {
+
+
+    public PlaylistAdapter(@NonNull Context context, int resource, @NonNull List<Playlist> objects) {
+        super(context, resource, objects);
+    }
+
+    class ViewHolder{
+        TextView txttenplaylist;
+        ImageView imgplaylist, imgbackground;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if(convertView==null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.dong_playlist,null);
+            viewHolder = new ViewHolder();
+            viewHolder.txttenplaylist = convertView.findViewById(R.id.textviewtenplaylist);
+            viewHolder.imgbackground = convertView.findViewById(R.id.imgbackgroudplaylist);
+            viewHolder.imgplaylist = convertView.findViewById(R.id.imgiconplaylist);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder= (ViewHolder) convertView.getTag();
+        }
+        Playlist playlist = getItem(position);
+        Picasso.with(getContext()).load(playlist.getHinhanh()).into(viewHolder.imgbackground);
+        Picasso.with(getContext()).load(playlist.getIcon()).into(viewHolder.imgplaylist);
+        viewHolder.txttenplaylist.setText(playlist.getTen());
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/appnghenhac/Model/Playlist.java
@@ -0,0 +1,53 @@
+package com.example.appnghenhac.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Playlist{
+
+@SerializedName("idplaylist")
+@Expose
+private String idplaylist;
+@SerializedName("ten")
+@Expose
+private String ten;
+@SerializedName("hinhanh")
+@Expose
+private String hinhanh;
+@SerializedName("icon")
+@Expose
+private String icon;
+
+public String getIdplaylist() {
+return idplaylist;
+}
+
+public void setIdplaylist(String idplaylist) {
+this.idplaylist = idplaylist;
+}
+
+public String getTen() {
+return ten;
+}
+
+public void setTen(String ten) {
+this.ten = ten;
+}
+
+public String getHinhanh() {
+return hinhanh;
+}
+
+public void setHinhanh(String hinhanh) {
+this.hinhanh = hinhanh;
+}
+
+public String getIcon() {
+return icon;
+}
+
+public void setIcon(String icon) {
+this.icon = icon;
+}
+
+}
